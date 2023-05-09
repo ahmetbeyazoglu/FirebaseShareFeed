ee827c404c354f71e49807404dab5b0ef5295a7e
herpestes <ahmetbeyazoglu@gmail.com>
2023-05-09 20:52:05 +0300

    Upload permission for the gallery

--- app/src/main/java/com/herpestes/firebaseinstagram/UploadActivity.kt
+++ app/src/main/java/com/herpestes/firebaseinstagram/UploadActivity.kt
@@ -1,17 +1,82 @@
 package com.herpestes.firebaseinstagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.herpestes.firebaseinstagram.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUploadBinding
-
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    var selectedPicture : Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        registerLauncher()
     }
+    fun upload(view: View){
+
+
+    }
+    fun selectedImage(view: View){
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                    //request permission
+                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }.show()
+            }else{
+                //request permission
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else{
+            val intenToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //start activity for result
+            activityResultLauncher.launch(intenToGallery)
+        }
+
+    }
+    private fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+            if(result.resultCode == RESULT_OK){
+                val intentFromResult = result.data
+                if(intentFromResult != null){
+                    selectedPicture = intentFromResult.data
+                    selectedPicture?.let {
+                        binding.imageView.setImageURI(it)
+                    }
+                }
+            }
+
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){result ->
+            if(result){
+                //permission gallery
+                val intenToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intenToGallery)
+
+            }else{
+                //permission needed
+                Toast.makeText(this,"Permission needed",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
 }
